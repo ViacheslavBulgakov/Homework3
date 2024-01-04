62a2f6a89e1af2e62151d0abe36dfb42067e4fa9
kkurs@mail.ru <kkurs@mail.ru>
2024-01-04 19:09:19 +0700

    исправлено задание 3

--- src/Main.java
+++ src/Main.java
@@ -45,20 +45,21 @@ public class Main {
         System.out.println("     задача_4");
 
     byte productivityTwuMinutes = 16;
+    int productivityOneMinute = productivityTwuMinutes/2;
     byte timeMinutes = 20;
     byte timeDay = 1;
     byte timeDays = 3;
     byte timeMonth = 1;
-    int produced1 = productivityTwuMinutes / 2 * 20;
-    System.out.println("За " + timeMinutes + " минут машина произвела "
+        int produced1 = productivityOneMinute * timeMinutes;
+        System.out.println("За " + timeMinutes + " минут машина произвела "
             + produced1 + " штук бутылок.");
-    int produced2 = productivityTwuMinutes / 2 * timeDay * 24 * 60;
+        int produced2 = productivityOneMinute * timeDay * 24 * 60;
         System.out.println("За " + timeDay + " день машина произвела "
                 + produced2 + " штук бутылок.");
-    int produced3 = productivityTwuMinutes / 2 * timeDays * 24 * 60;
+        int produced3 = produced2 * timeDays;
         System.out.println("За " + timeDays + " дня машина произвела "
                 + produced3 + " штук бутылок.");
-    int produced4 = productivityTwuMinutes / 2 * timeMonth * 30 * 24 * 60;
+        int produced4 = produced3 * 10;
         System.out.println("За " + timeMonth + " месяц машина произвела "
                 + produced4 + " штук бутылок.");
 
@@ -67,37 +68,38 @@ public class Main {
     byte canTotal = 120;
     byte canWhiteClass = 2;
     byte canBrownClass = 4;
-    byte numberClasses = (byte) (canTotal/(canBrownClass+canWhiteClass));
-    byte canWhiteTotal = (byte) (numberClasses * canWhiteClass);
-    byte canBrownTotal  = (byte) (numberClasses * canBrownClass);
+    int numberClasses = canTotal/(canBrownClass+canWhiteClass);
+    int canWhiteTotal = numberClasses * canWhiteClass;
+    int canBrownTotal  = numberClasses * canBrownClass;
     System.out.println("В школе, где " + numberClasses + " классов, нужно "
             + canWhiteTotal + " банок белой краски и " + canBrownTotal +
             " банок коричневой краски.");
 
     System.out.println("     задача_6");
 
-short numberBananas = 5;
-short weightBanana = 80;
-short numberMilk = 200;
-short weight100Milk = 105;
-short numberIceCream = 2;
-short weightIceCream = 100;
-short numberEggs = 4;
-short weightEggs = 70;
-float weightBreakfastGr = (float) ((numberBananas * weightBanana) + ((numberMilk / 100) * weight100Milk) +
+int numberBananas = 5;
+int weightBanana = 80;
+int numberMilk = 200;
+int weight100Milk = 105;
+int numberIceCream = 2;
+int weightIceCream = 100;
+int numberEggs = 4;
+int weightEggs = 70;
+    float weightBreakfastGr = (float) ((numberBananas * weightBanana) + ((numberMilk / 100) * weight100Milk) +
         (numberIceCream * weightIceCream) + (numberEggs * weightEggs));
-float weightBreakfastKgr = weightBreakfastGr/1000;
-System.out.println("Вес спортзавтрака составляет " + weightBreakfastGr + " граммов или " +
+    float weightBreakfastKgr = weightBreakfastGr/1000;
+        System.out.println("Вес спортзавтрака составляет " + weightBreakfastGr + " граммов или " +
         weightBreakfastKgr + " килограмма.");
 
         System.out.println("     задача_7");
 
         float weightTotal = 7;
+        float weightTotalGr = weightTotal * 1000;
         float weightDayMin = 250;
         float weightDayMax = 500;
-        float numberDaysMin = weightTotal * 1000 / weightDayMax;
-        float numberDaysMax = weightTotal * 1000 / weightDayMin;
-        float numberDaysMid = (weightTotal * 1000) /((weightDayMin + weightDayMax)/2);
+        float numberDaysMin = weightTotalGr / weightDayMax;
+        float numberDaysMax = weightTotalGr / weightDayMin;
+        float numberDaysMid = weightTotalGr /((weightDayMin + weightDayMax)/2);
         System.out.println("Если спортсмен будет терять по " + weightDayMin + " грамм в день, то чтобы сбросить " +
                 weightTotal + " килограмм, ему потребуется " + numberDaysMax + " дней, если по " +
                 weightDayMax + " грамм, то " + numberDaysMin + " дней. В среднем ему потребуется " +
@@ -108,12 +110,21 @@ System.out.println("Вес спортзавтрака составляет " + w
         int wageM = 67760;
         int wageD = 83690;
         int wageK = 76230;
-        System.out.println("Маша теперь получает " + wageM * 1.1 + " рублей. Годовой доход вырос на " +
-                wageM * 0.1 * 12 + " рублей.");
-        System.out.println("Денис теперь получает " + wageD * 1.1 + " рублей. Годовой доход вырос на " +
-                wageD * 0.1 * 12 + " рублей.");
-        System.out.println("Кристина теперь получает " + wageK * 1.1 + " рублей. Годовой доход вырос на " +
-                wageK * 0.1 * 12 + " рублей.");
+        float factorIncrease = 0.1f; // Коэфициент повышения заплаты
+        double wageMnew = wageM * (1 + factorIncrease); // Зарплата Маши после повышения
+        double wageDnew = wageD * (1 + factorIncrease);
+        double wageKnew = wageK * (1 + factorIncrease);
+        double increaseAnnualSalaryM = wageM * factorIncrease * 12; // Годовое повышение зарплаты у Маши
+        double increaseAnnualSalaryD = wageD * factorIncrease * 12;
+        double increaseAnnualSalaryK = wageK * factorIncrease * 12;
+
+
+        System.out.println("Маша теперь получает " + wageMnew + " рублей. Годовой доход вырос на " +
+                increaseAnnualSalaryM + " рублей.");
+        System.out.println("Денис теперь получает " + wageDnew + " рублей. Годовой доход вырос на " +
+                increaseAnnualSalaryD + " рублей.");
+        System.out.println("Кристина теперь получает " + wageKnew + " рублей. Годовой доход вырос на " +
+                increaseAnnualSalaryK + " рублей.");
 
 
 
